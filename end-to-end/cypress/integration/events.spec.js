c50f6a1b132a467dde69cb959104de63c8880e7b
Ricardo Borillo <borillo@gmail.com>
2020-02-26 19:02:22 +0100

    Extracted pageobject methons in-class

--- end-to-end/cypress/integration/events.spec.js
+++ end-to-end/cypress/integration/events.spec.js
@@ -1,23 +1,58 @@
+function goToHomePage() {
+  cy.visit('/');
+}
+
+function openLoginForm() {
+  cy.get('#navbarResponsive')
+    .contains('Account')
+    .click();
+  cy.get('#navbarResponsive .dropdown-menu')
+    .contains('Sign in')
+    .click();
+}
+
+function signInAsAdmin() {
+  goToHomePage();
+  openLoginForm();
+
+  cy.get('#username').type('admin');
+  cy.get('#password').type('admin');
+  cy.get('.modal-body')
+    .contains('Sign in')
+    .click();
+}
+
+function createNewEvent({ name, startsAt, duration }) {
+  cy.contains('Entities').click();
+  cy.contains('Event').click();
+
+  cy.get('#page-heading')
+    .contains('Create a new Event')
+    .click();
+
+  cy.get('#field_name').type(name);
+  cy.get('#field_startsAt').type(startsAt);
+  cy.get('#field_duration').type(duration);
+
+  cy.get('#save-entity').click();
+}
+
+function assertEventHasBeenCreated() {
+  cy.get('.alerts .alert-success');
+}
+
 describe('Event entity', () => {
-    it('can be created', () => {
-        cy.visit('http://localhost:8080')
-        cy.get('#navbarResponsive').contains('Account').click()
-        cy.get('#navbarResponsive .dropdown-menu').contains('Sign in').click()
+  beforeEach(() => {
+    signInAsAdmin();
+  });
 
-        cy.get('#username').type('admin')
-        cy.get('#password').type('admin')
-        cy.get('.modal-body').contains('Sign in').click()
+  it('can be created', () => {
+    createNewEvent({
+      name: 'Test',
+      startsAt: '2020-02-27',
+      duration: 4
+    });
 
-        cy.contains('Entities').click()
-        cy.contains('Event').click()
-        cy.get('#page-heading').contains('Create a new Event').click()
-        
-        cy.get('#field_name').type('Test')
-        cy.get('#field_startsAt').type('2020-02-27')
-        cy.get('#field_duration').type(4)
-
-        cy.get('#save-entity').click()
-
-        cy.get('.alerts .alert-success').should('exist')
-    })
-})
+    assertEventHasBeenCreated();
+  });
+});
